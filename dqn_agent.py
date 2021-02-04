4b03a4352b0b74b5cbcb09797af74b20a875ec4a
EdoSol <edoardo.solis@gmail.com>
2021-02-04 04:13:05 +0000

    Initial Commit

--- dqn_agent.py
+++ dqn_agent.py
@@ -10,10 +10,10 @@ import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size (default: 64)
-GAMMA = 0.9965          # discount factor (default: 0.99)
+GAMMA = 0.9990          # discount factor (default: 0.99)
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
-UPDATE_EVERY = 4        # how often to update the network
+UPDATE_EVERY = 5        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
